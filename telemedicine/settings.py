20f651500f8b158f67fec569d010e73f4dcb0b0b
abaczyk <s180489@student.pg.edu.pl>
2022-04-19 11:00:20 +0200

    Added django_heroku

--- telemedicine/settings.py
+++ telemedicine/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-88u*zj0sfp#p6@dbmj)tlu0yq3b$uzl45*1_7_*cxu35n#5(ew'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['teleporada.herokuapp.com', '127.0.0.1']
 
@@ -138,3 +139,5 @@ STATICFILES_DIRS=[os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
+
